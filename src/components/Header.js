1392ca06a99524f3bfac6ca4892577c2df4093ad
Sharon Paz <oxleberry@gmail.com>
2020-04-20 10:18:37 -0700

    Adding responsive styles for smaller screens.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const header = () => {
     return (
         <div className="Header">
-            <h1>The Ultimate East Bay Takout Guide: Mapped!</h1>
+            <h1>The Ultimate East Bay Takeout Guide: Mapped!</h1>
             <a href="https://www.eastbayexpress.com/oakland/the-ultimate-east-bay-takeout-guide/Content?oid=29466571" target="blank">Link to the original article in the East Bay Express &#8663;</a>
         </div>
     )
